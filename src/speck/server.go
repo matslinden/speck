aa590b957a367387200ca18a92d8e8f0227603c5
Astrids8d <astrids8d@users.noreply.github.com>
2016-08-15 14:41:46 +0200

    Fixing issue Origin not allowed
    Allows router to be used from external client
    see https://github.com/jcelliott/turnpike/issues/73

--- src/speck/server.go
+++ src/speck/server.go
@@ -11,8 +11,12 @@ import (
 var port = flag.String("port", "8080", "port to run the server on")
 
 func main() {
+	turnpike.Debug()
+
 	flag.Parse()
 	ws := turnpike.NewBasicWebsocketServer("realm1")
+	allowAllOrigin := func(r *http.Request) bool { return true }
+	ws.Upgrader.CheckOrigin = allowAllOrigin
 	http.Handle("/ws", ws)
 	http.Handle("/", http.FileServer(http.Dir("web")))
 
